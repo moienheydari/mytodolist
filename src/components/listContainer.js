35270b60167891c56ea975e005c7d42259167d86
Moien <137896500+moienheydari@users.noreply.github.com>
2023-07-02 16:14:26 +0330

    Revert "temp ok"
    This reverts commit 86ec6cb35ef332a37b21025cbc19f078e5903a32.

--- src/components/listContainer.js
+++ src/components/listContainer.js
@@ -3,19 +3,13 @@ import ListField from './listField';
 import data from '../data/data';
 import '../css/listCont.css';
 
-export default function ListContainer ({handleUpdate}) {
-    function changeDone(index, bool) {
-        data.tasks[index].done = bool;
-        handleUpdate();
-    }
-
+export default function ListContainer () {
     return (
         <>
             <div className='list-cont'>
                 {
-                data.tasks.map((e, index) => {
-                    return <ListField task={e.task} subtask={e.subTask} done={e.done} 
-                                taskIndex={index} changeDone={changeDone}/>;
+                data.tasks.map(e => {
+                    return <ListField task={e.task} subtask={e.subTask} done={e.done}/>;
                 })
                 }
             </div>
--- src/components/listField.js
+++ src/components/listField.js
@@ -1,13 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import '../css/listCont.css';
 
-export default function ListField({ task, subtask, done, taskIndex, changeDone }) {
+export default function ListField({ task, subtask, done }) {
     const [clicked, setClicked] = useState(false);
-    const [strike, setStrike] = useState(done);
-
-    useEffect(()=>{
-        changeDone(taskIndex, strike);
-    },[strike]);
+    const [strike, setStrike] = useState(done)
 
     function handleDoneClick() {
         setStrike((prev) => { return !prev });
@@ -20,18 +16,12 @@ export default function ListField({ task, subtask, done, taskIndex, changeDone }
         }
     }
 
-    function handleDelClick() {
-
-    }
-
     return (
         <div className='list-field'>
             <div className='task-field' onClick={handleTaskClick}>
                 <span className={`task-name ${strike ? 'strikethrough' : ''}`}>{task}</span>
-                <button className={`taskButt ${strike ? 'strikethrough' : ''}`} onClick={handleDoneClick}>
-                    Done</button>
-                <button className='taskButt' onClick={handleDelClick}>
-                    Del.</button>
+                <button className={`taskButt ${strike ? 'strikethrough' : ''}`} onClick={handleDoneClick}>Done</button>
+                <button className='taskButt'>Del.</button>
             </div>
             <div className={`subtask-detail ${clicked ? 'show' : 'hide'} ${strike ? 'strikethrough' : ''}`}>
                 <span className='subtask-text'>{subtask}</span>
